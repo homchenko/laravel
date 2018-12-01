3b9131193aeaf25f5ddecd4aecb034350bc8ed77
Web <web@it.step>
2018-12-01 21:01:48 +0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $title = 'title';
+        return view('posts.index', compact($title));
+    }
+}
